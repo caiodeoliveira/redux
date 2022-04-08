45ad1a455f7202c462afaa7e1e2b863138a95619
Caio Ribeiro <Caio.ribeiro98@outlook.com>
2022-04-08 12:59:29 -0300

    🔥 Cleaned some code and remaked redux store imports

--- src/services/store/index.ts
+++ src/services/store/index.ts
@@ -1,10 +1,1 @@
-import { createStore, compose, applyMiddleware } from "redux";
-import rootReducer from "./ducks/rootReducer";
-import { Course } from "./ducks/Course/types";
-
-// export interface ApplicationState {
-//     Course: CourseState;
-//     Promotions: PromotionsState;
-// }
-
-const store = createStore(rootReducer);
+import { createStore, applyMiddleware, Store, AnyAction } from "redux";
